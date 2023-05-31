676f94a61b66206051dedc49eb701fa7559d2b78
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-05-31 10:39:12 -0300

    Final

--- Componentes/Formulario.jsx
+++ Componentes/Formulario.jsx
@@ -1,26 +1,31 @@
-import React, {useState} from "react"
-import {SafeAreaView, Button, TextInput, Text, StyleSheet} from "react-native"
+import React, {useState} from "react";
+import {SafeAreaView, Button, TextInput, Text, StyleSheet, text} from "react-native";
+import { stringifyValueWithProperty } from "react-native-web/dist/cjs/exports/StyleSheet/compiler";
+
 const Formulario = () => {
-    const [text, onChangeText] = React.useState("ingrese texto")
+    const [text, onChangeText] = React.useState("")
+
     return(
         <SafeAreaView style={styles.container}>
             <div className="cuadrado" View style={styles.cuadrado}>
-            <TextInput
-            style={styles.input}
-            onChangeText={onChangeText}
-            value={text}
-            />
-            <Button
-            title= "PRESIONAME"
-            color="#f194ff"
-            />
+            <TextInput style={styles.input} onChangeText={onChangeText} placeholder ="ingrese" value={text}/>
+            <Text>{text.length}</Text>
+            <Button title= "PRESIONAME" color="#f194ff"/>
+            <hr/>
+            <Text>{text}</Text>
             </div>
         </SafeAreaView>
+
     )
-}
+};
+
 const styles = StyleSheet.create({
     input:{
-        height:30
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        color: 'grey'
     },
     cuadrado:{
         height:300,
@@ -31,6 +36,8 @@ const styles = StyleSheet.create({
         borderWidth:5,
         borderColor: 'gray',
         borderWidth: 2.5,
-    }
-})
-export default Formulario
+    },
+
+});
+
+export default Formulario;
